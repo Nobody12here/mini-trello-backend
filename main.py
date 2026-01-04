c4ea8f496651330eb304d9a9b23503b97786743d
Nobody12here <uraja01313@gmail.com>
2026-01-04 18:28:13 +0500

    added apps for the project and added swagger docs

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mini-trello!")
-
-
-if __name__ == "__main__":
-    main()
--- minitrello/urls.py
+++ minitrello/urls.py
@@ -1,23 +1,20 @@
-"""
-URL configuration for minitrello project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+schema_view = get_schema_view(
+    openapi.Info(
+        "mini-trello",
+        "1.0.0",
+        "A RESTful backend system where users can create projects, manage tasks, assign tasks to team members, and track task status.",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/docs/", schema_view.with_ui("swagger", 0), name="api-docs"),
 ]
