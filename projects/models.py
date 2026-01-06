d29d78a5de26902c4067d60286d1ee0356ef3e58
Nobody12here <uraja01313@gmail.com>
2026-01-06 11:41:16 +0500

    added Porject model

--- projects/models.py
+++ projects/models.py
@@ -1,2 +1,14 @@
 from django.db import models
+from accounts.models import CustomUserModel
 
+
+class Project(models.Model):
+    name = models.CharField(max_length=60)
+    descripiton = models.TextField()
+    owner = models.ForeignKey(
+        CustomUserModel, related_name="project_owner", on_delete=models.CASCADE
+    )
+    members = models.ManyToManyField(CustomUserModel, related_name="project_members")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
