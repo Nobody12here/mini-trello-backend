6d793c8c844723342e0e9fd7195387043ea2e386
Nobody12here <uraja01313@gmail.com>
2026-01-07 13:29:53 +0500

    Add ProjectMemberSerializer and implement add_members action in ProjectViewSet

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from accounts.models import CustomUserModel
 from .models import Project
 
 
@@ -6,3 +7,19 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class ProjectMemberSerializer(serializers.ModelSerializer):
+    members = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUserModel.objects.all(), many=True
+    )
+
+    class Meta:
+        model = Project
+        fields = ["members"]
+
+    def update(self, instance, validated_data):
+        members = validated_data.get("members", [])
+        if members:
+            instance.members.add(*members)
+        return instance
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.decorators import action
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectMemberSerializer
 from .models import Project
 
 # Create your views here.
@@ -10,3 +14,12 @@ class ProjectViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+    @action(detail=True, methods=["post"])
+    def add_members(self, request: Request, pk=None):
+        project = self.get_object()
+        serializer = ProjectMemberSerializer(project, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response({"message": "Adding member"}, status=status.HTTP_200_OK)
