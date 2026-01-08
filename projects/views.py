a37826ebc850a5a3811be554ecd0271d56937460
Nobody12here <uraja01313@gmail.com>
2026-01-08 11:33:02 +0500

    Enhance ProjectViewSet to enforce project ownership checks; improve member management actions and fix typos in response messages

--- projects/views.py
+++ projects/views.py
@@ -4,21 +4,34 @@ from rest_framework.request import Request
 from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .serializers import ProjectSerializer, ProjectMemberSerializer
 from .models import Project
 from accounts.models import CustomUserModel
-
-# Create your views here.
 
 
 class ProjectViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
+
+    def get_queryset(self):
+        owner = self.request.user
+        return Project.objects.filter(owner=owner)
+        
+
+    def _check_project_owner(self, project: Project):
+        if project.owner != self.request.user:
+            raise PermissionDenied("Current User is not the owner of this project!")
+
+    def perform_create(self, serializer: ProjectSerializer):
+        project: Project = serializer.save(owner=self.request.user)
+        project.members.add(project.owner)
+        return project
 
     @action(detail=True, methods=["post"])
     def add_members(self, request: Request, pk=None):
-        project = self.get_object()
+        project: Project = self.get_object()
+        self._check_project_owner(project=project)
         serializer = ProjectMemberSerializer(
             instance=project, data=request.data, partial=True
         )
@@ -27,21 +40,26 @@ class ProjectViewSet(ModelViewSet):
 
         return Response(
             {
-                "message": "member added sucessfully",
+                "message": "member added successfully",
             },
-            status=status.HTTP_200_OK,
+            status=status.HTTP_204_NO_CONTENT,
         )
 
     @action(detail=True, methods=["post"])
     def remove_members(self, request: Request, pk=None):
         project: Project = self.get_object()
+        self._check_project_owner(project=project)
         members = request.data.get("members", [])
         if members:
-            members_ids = CustomUserModel.objects.filter(id__in=members).exclude(
+            member_ids = CustomUserModel.objects.filter(id__in=members).exclude(
                 id=project.owner.id
             )
-            project.members.remove(*members_ids)
-
+            project.members.remove(*member_ids)
+        else:
+            return Response(
+                {"error": "No member ids provided"}, status=status.HTTP_400_BAD_REQUEST
+            )
         return Response(
-            {"message": "Members removed sucessfully"}, status=status.HTTP_200_OK
+            {"message": "Members removed successfully"},
+            status=status.HTTP_204_NO_CONTENT,
         )
